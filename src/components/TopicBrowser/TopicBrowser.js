a2b3489a16f85b31e77a239ad91bed0da629a5e8
Thang Khai <tnkhai94@gmail.com>
2020-08-18 15:55:08 -0600

    initial commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+class TopicBrowser extends Component {
+    render() {
+        return (
+            <p> Hello World</p>
+        )
+    }
+}
+
+export default TopicBrowser;
